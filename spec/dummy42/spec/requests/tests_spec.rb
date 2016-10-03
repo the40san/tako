5de6fd24fa823d39a7b8b0e3f73fc8931a8202b9
Masashi AKISUE <masashi.akisue@aktsk.jp>
2016-10-03 10:51:11 +0900

    add rails 4.2 spec

--- spec/dummy42/spec/requests/tests_spec.rb
+++ spec/dummy42/spec/requests/tests_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe "Tests", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /tests" do
+    it "creates a record to shard02" do
+      post tests_path
+
+      expect(Test.shard(:shard01).count).to eq(0)
+      expect(Test.shard(:shard02).count).to eq(1)
+      expect(Test.count).to eq(0)
+    end
+  end
 end
